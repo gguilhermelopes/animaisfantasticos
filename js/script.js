da0a2f0b973251e74abc97dc23da746074a65da3
gguilhermelopes <guilhermelopes275@gmail.com>
2022-10-27 15:53:20 -0300

    fetch-animais-refatorado

--- js/script.js
+++ js/script.js
@@ -1,13 +1,13 @@
 import ScrollSuave from "./modules/scroll-suave.js";
-import initAnimacaoScroll from "./modules/animacao-scroll.js";
 import Accordion from "./modules/accordion.js";
 import TabNav from "./modules/tab-nav.js";
 import Modal from "./modules/modal.js";
 import Tooltip from "./modules/tooltip.js";
+import fetchAnimais from "./modules/fetch-animais.js";
+import initAnimacaoScroll from "./modules/animacao-scroll.js";
 import initMenuDropdown from "./modules/menu-dropdown.js";
 import initMenuMobile from "./modules/menu-mobile.js";
 import initFuncionamento from "./modules/funcionamento.js";
-import initFetchAnimais from "./modules/fetch-animais.js";
 import initFetchBtc from "./modules/fetch-btc.js";
 
 const scrollSuave = new ScrollSuave('[data-menu="suave"] a[href^="#"]');
@@ -32,9 +32,9 @@ modal.init();
 const tooltip = new Tooltip("[data-tooltip]");
 tooltip.init();
 
+fetchAnimais("../../animaisapi.json", ".numeros-grid");
 initAnimacaoScroll();
 initMenuDropdown();
 initMenuMobile();
 initFuncionamento();
-initFetchAnimais();
 initFetchBtc();
